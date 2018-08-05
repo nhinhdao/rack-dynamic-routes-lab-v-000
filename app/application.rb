078ee0f6a69c77dc8641aa78c784ed92b6ffca6b
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-05 18:18:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,9 @@ class Application
 
     @@items = [Item.new("Macbook", 1200), Item.new("Bose", 350)]
 
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
     def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
         if req.path.match(/items/)
             item_name = req.path.split("items").last
             if item = @@items.find {|i| i.name = item_name}
