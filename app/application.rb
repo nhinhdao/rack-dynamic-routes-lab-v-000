6bc57051c307a05c1aff25df4ea7ad0a3d5773e5
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-08-05 18:18:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,18 +4,19 @@ class Application
 
     resp = Rack::Response.new
     req = Rack::Request.new
-
-    if req.path.match(/items/)
-        item_name = req.path.split("items").last
-        if item = @@items.find {|i| i.name = item_name}
-            resp.write "#{item.price}"
+    def call(env)
+        if req.path.match(/items/)
+            item_name = req.path.split("items").last
+            if item = @@items.find {|i| i.name = item_name}
+                resp.write "#{item.price}"
+            else
+                resp.write "Item not found"
+                resp.status = 400
+            end
         else
-            resp.write "Item not found"
-            resp.status = 400
+            resp.write "Route not found"
+            resp.status = 404
         end
-    else
-        resp.write "Route not found"
-        resp.status = 404
+        resp.finish
     end
-    resp.finish
 end
